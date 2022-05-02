a94cb4b3f2430418d5276098cf4d7afc415ae357
Matthew Ranson <matthew.ranson2718@gmail.com>
2022-05-02 21:54:26 +0100

    debugging collision kernel

--- CUDA/kernel.cu
+++ CUDA/kernel.cu
@@ -66,19 +66,22 @@ __device__ float intersection(int ax, int ay, int bx, int by, int cx, int cy, in
 }
 
 __device__ float collidex(int width, Ray r, int bs, bool *grid, int step){
+    printf("IN COLX %d \n", step);
     int c = r.ox + (bs / 2) * step;
     int y;
     bool collided = false;
     while ((c < ((width - 1) * bs)) && (c > (bs)) && (!collided)){
         y = round(r.m * c + r.c);
-        int y2 = y / bs;
+        int y2 = floor((float)y / bs);
         if (grid[width * y2 + c]){ collided = true; }
         c += step;
     }
+    printf("RETURN\n");
     return sqrt((float)(y * y) + ((c * bs) * (c * bs)));
 }
 
 __device__ float collidey(int width, int height, Ray r, int bs, bool *grid, int step){
+    printf("IN COLY %d \n", step);
     int c = r.oy + (bs / 2) * step;
     int x;
     bool collided = false;
@@ -90,34 +93,42 @@ __device__ float collidey(int width, int height, Ray r, int bs, bool *grid, int 
     }
     float dist = sqrt((float)(x * x) + ((c * bs) * (c * bs)));
     printf("%f\n", dist);
-    printf("dhfiosfhgo");
+    //printf("dhfiosfhgo");
     return dist;
 }
 
 
 __global__ void collisions(int wGrid, int nRays, int bs, Ray *rays, bool *grid, float *cols){
-    printf("IN KERNEL");
+    
     int index = blockIdx.x * blockDim.x + threadIdx.x;
+    //printf("IN KERNEL%d\n", index);
     int stride = blockDim.x * gridDim.x;
     for (int i = index; i < nRays; i += stride){
+        printf("BEFORE RAY\n");
         Ray r = rays[i];
+        printf("AFTER RAY\n");
         //float closest = 10000000.0;
-        float closestx = 10000000;
-        float closesty = 10000000;
+        float closestx = 100;
+        float closesty = 10;
+        
         if ((r.ox - r.px) > 1){
             closestx = collidex(wGrid, r, bs, grid, -1);
         } else{ 
             closestx = collidex(wGrid, r, bs, grid, 1);
         }
-        int hGrid = (sizeof(grid)/sizeof(*grid))/wGrid;
+        //int hGrid = (sizeof(grid)/sizeof(*grid))/wGrid;
+        printf("OUT OF X\n");
+        int hGrid = 30;
+        printf("GOING TO Y\n");
         if ((r.oy - r.py) > 1){
             closesty = collidey(wGrid, hGrid, r, bs, grid, -1);
         } else{ 
             closesty = collidey(wGrid, hGrid, r, bs, grid, 1);
         }
-
-        if (closestx < closesty) { cols[i] = closestx; }
+        
+        if (closestx <= closesty) { cols[i] = closestx; }
         else { cols[i] = closesty; }
+        //cols[i] = (float)i;
         //printf("%f\n", closestx);
         //printf("hdihfguhfug");
     }
@@ -129,35 +140,35 @@ void getCollisionDistance(int wGrid, int bs, int nRays, Ray *rays, bool *grid, f
     //printf("%d\n", N);
     
     float *cols;
-    printf("MALLOC COLS\n");
+    //printf("MALLOC COLS\n");
     cudaMallocManaged(&cols, N*sizeof(float));
     Ray *rs;
-    printf("MALLOC RS\n");
+    //printf("MALLOC RS\n");
     cudaMallocManaged(&rs, N*sizeof(Ray));
-    printf("SET RAYS\n");
+    //printf("SET RAYS\n");
     for (int i = 0; i < N ; i++){
         rs[i] = rays[i];
         cols[i] = 0.0;
     }
-    printf("BLOCKSIZE\n");
+    //printf("BLOCKSIZE\n");
     int blockSize = 256;
-    printf("NUMBLOCKS\n");
+    //printf("NUMBLOCKS\n");
     int numBlocks = (N + blockSize - 1) / blockSize;
-    printf("GOING INTO KERNEL\n");
+    //printf("GOING INTO KERNEL\n");
     collisions<<<numBlocks, blockSize>>>(wGrid, N, bs, rs, grid, cols);
     cudaDeviceSynchronize();
-    printf("SYNCHRONIZED\n");
+    //printf("SYNCHRONIZED\n");
     
-    printf("LOOP\n");
+    //printf("LOOP\n");
     for (int i = 0; i < N ; i++){
-        //collis[i] = cols[i];
+        collis[i] = cols[i];
         //collis[i] = 100 + i;
-        //printf("%f\n", cols[i]);
-        collis[i] = 100;
+        printf("%f\n", cols[i]);
+        //collis[i] = 100;
     }
-    printf("OUT OF LOOP\n");
+    //printf("OUT OF LOOP\n");
     cudaFree(rs);
-    printf("FREEING\n");
+    //printf("FREEING\n");
     cudaFree(cols);
     //printf("FREED\n");
     //printf("odijoidfjgoijog");
